aaacdd2b0f0f773c0a0804055b37b1f79dc0c4cc
alex godin <alex@alexgodin.com>
2019-01-29 09:54:50 -0500

    add js

---
+++ uncompressed.js
@@ -0,0 +1,3 @@
+var labels = $('.all_labels').html
+$('body').html(labels)
+$('head').append('<link rel="stylesheet" type="text/css" media="print" href="https://raw.githubusercontent.com/alexgodin/bookmarklet/master/labels.css">');

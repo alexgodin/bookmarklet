fee59db898b8b302facd95740b14695730ca5265
alex godin <alex@alexgodin.com>
2019-01-29 10:09:11 -0500

    WIP

--- uncompressed.js
+++ uncompressed.js
@@ -1,3 +1,3 @@
-var labels = $('.all_labels').html;
+var labels = $('.all_labels').html();
 $('body').html(labels);
-$('head').append('<link rel="stylesheet" type="text/css" media="print" href="https://raw.githubusercontent.com/alexgodin/bookmarklet/master/labels.css">');
+$('head').append('<link rel="stylesheet" type="text/css" href="https://raw.githubusercontent.com/alexgodin/bookmarklet/master/labels.css">');
